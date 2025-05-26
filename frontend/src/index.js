805f2ac57bdafbb7064ca7dc084218a5286a2511
victor804 <victor.vaucel@gmail.com>
2025-05-26 15:04:23 +0200

    feat: add router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+const router = createBrowserRouter([
+    {path:"/", element: <App/>}
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <StrictMode>
+        <RouterProvider router={router}/>
+    </StrictMode>
+
+);
